2ad8dbb897a645d9755232d5477ac573d1f7786a
Тимошевский Петр <best_petrovich@mail.ru>
2017-07-18 07:27:13 +0300

    ~ Refactoring

--- ElectorCsvParser/ElectorCsvParser/AreaParser.cs
+++ ElectorCsvParser/ElectorCsvParser/AreaParser.cs
@@ -20,35 +20,36 @@ namespace ElectorCsvParser
                 var indx = str.IndexOf(':');
                 var addrStr = str.Substring(0, indx).ToUpper();
 
-                string[] markers = Markers.CreateStreetMarkers();
-                bool gotStreet = false;
-                foreach (var marker in markers)
+                var street = GetStreet(addrStr);
+                if (!_streetHouses.ContainsKey(street))
+                    _streetHouses.Add(street, new List<House>());
+            }
+        }
+
+        private Street GetStreet(string addrStr)
+        {
+            string[] markers = Markers.CreateStreetMarkers();
+            foreach (var marker in markers)
+            {
+                var markerPos = addrStr.IndexOf(marker);
+                if (markerPos < 0)
+                    continue;
+
+                Street street;
+                if (markerPos == 0)
                 {
-                    var markerPos = addrStr.IndexOf(marker);
-                    if (markerPos < 0)
-                        continue;
-
-                    gotStreet = true;
-                    Street street;
-                    if (markerPos == 0)
-                    {
-                        var shortName = addrStr.Substring(markerPos+marker.Length).Trim(CsvParser.TrimChars);
-                        street = new Street(shortName, marker);
-                    }
-                    else
-                    {
-                        street = CsvParser.ParseStreet(addrStr);
-                    }
-
-                    if(!_streetHouses.ContainsKey(street))
-                        _streetHouses.Add(street, new List<House>());
-
-                    break;
+                    var shortName = addrStr.Substring(markerPos + marker.Length).Trim(CsvParser.TrimChars);
+                    street = new Street(shortName, marker);
+                }
+                else
+                {
+                    street = CsvParser.ParseStreet(addrStr);
                 }
 
-                if (!gotStreet)
-                    throw new Exception(string.Format("Cant parse Street: {0}", addrStr));
+                return street;
             }
+
+            throw new Exception(string.Format("Cant parse Street: {0}", addrStr));
         }
     }
 }
